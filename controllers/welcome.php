a4b7d6b39870d04dcb757c6821d5e288b9dea4d8
Linnu <linda.tanissaar@khk.ee>
2017-02-14 18:18:28 +0200

    work in progress

--- controllers/welcome.php
+++ controllers/welcome.php
@@ -3,20 +3,8 @@
 class welcome extends Controller
 {
 
-    function index()
-    {
-        $this->users = get_all("SELECT * FROM users");
+    function index(){
+
     }
 
-    function AJAX_index()
-    {
-        echo "\$_POST:<br>";
-        var_dump($_POST);
-    }
-
-    function POST_index()
-    {
-        echo "\$_POST:<br>";
-        var_dump($_POST);
-    }
 }
